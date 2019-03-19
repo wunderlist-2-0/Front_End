530a9f5621ebd610e94af0023f4bb2ecf53e5eef
x-zen <jdharris.x@gmail.com>
2019-03-19 14:36:21 -0500

    Updates

--- zen-front-end/src/components/List/List.js
+++ zen-front-end/src/components/List/List.js
@@ -5,8 +5,12 @@ import Todo from './Todo.js';
 class List extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="list">
         <Todo />
+        <div className='new-list'>
+          <h2>Create a new list...</h2>
+          <i class="fas fa-plus-circle"></i>
+        </div>
       </div>
     );
   }
